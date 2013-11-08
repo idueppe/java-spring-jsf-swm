4a128f3a4874e924a8cc23bd47584cacac9e4308
idueppe <ingo.dueppe@dueppe.com>
2013-11-08 14:12:21 +0100

    Add JDBC Test for ProductRepositoryJdbcBean

---
+++ speedauction-core/src/test/java/de/swm/auction/dao/jdbc/ProductRepositoryTest.java
@@ -0,0 +1,67 @@
+package de.swm.auction.dao.jdbc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import de.swm.auction.dao.ProductRepository;
+import de.swm.auction.exceptions.ProductNotFoundException;
+import de.swm.auction.model.ProductDetails;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext-db.xml")
+public class ProductRepositoryTest
+{
+
+	@Autowired
+	private ProductRepository productRepository; 
+	
+
+	@Test
+	public void testThatProductIdIsSetAfterPersist()
+	{
+		ProductDetails product = new ProductDetails();
+		assertNull("Id should be null before a domain object is persisted.",product.getId());
+		productRepository.persist(product);
+		assertNotNull("Id must not be null after a domain object is persisted.",product.getId());
+	}
+	
+	@Test
+	public void testThatProductCanBeFoundAfterPersist() throws ProductNotFoundException
+	{
+		ProductDetails product = new ProductDetails();
+		productRepository.persist(product);
+		ProductDetails found = productRepository.find(product.getId());
+		assertEquals(product, found);
+	}
+	
+	@Test(expected=ProductNotFoundException.class)
+	public void testThatIsDeletedAndCannotBeFoundAgain() throws ProductNotFoundException
+	{
+		ProductDetails product = new ProductDetails();
+		productRepository.persist(product);
+		productRepository.delete(product.getId());
+		productRepository.find(product.getId());
+	}
+	
+	@Test
+	public void testFindAllResultSize() throws Exception
+	{
+		int numberOfProducts = productRepository.findAll().size();
+		assertNotNull(productRepository.findAll());
+		productRepository.persist(new ProductDetails());
+		assertFalse(productRepository.findAll().isEmpty());
+		productRepository.persist(new ProductDetails());
+		assertEquals(numberOfProducts+2, productRepository.findAll().size());
+	}
+	
+	
+
+}
