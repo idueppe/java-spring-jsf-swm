bbffb3b2c9b27c553e827abc2d3aa533af82665b
idueppe <ingo.dueppe@dueppe.com>
2013-11-15 12:34:19 +0100

    Add example for Thread Race Condition

---
+++ speedauction-core/src/test/java/de/swm/auction/ThreadRaceConditionTest.java
@@ -0,0 +1,52 @@
+package de.swm.auction;
+
+import org.junit.Test;
+
+public class ThreadRaceConditionTest
+{
+
+	private int count;
+
+	private Runnable increase = new Runnable()
+	{
+
+		@Override
+		public void run()
+		{
+			for (int i = 0; i < 1_000_000_000; i++)
+			{
+				synchronized (ThreadRaceConditionTest.class) {
+					count++;
+				}
+			}
+			
+			System.out.println(Thread.currentThread().getName()+": "+count);
+		}
+	};
+
+	@Test
+	public void testRaceCondition() throws Exception
+	{
+		System.out.println(Integer.MIN_VALUE +" - "+Integer.MAX_VALUE);
+		
+		Thread a = new Thread(increase,"A");
+		Thread b = new Thread(increase,"B") {
+			
+			public void start() 
+			{
+				System.out.println(getName()+" wurde gestartet");
+				super.start();
+			}
+			
+		};
+		a.start();
+		b.start();
+
+		a.join();
+		b.join();
+		
+		System.out.println("Finised "+count);
+		
+	}
+
+}
