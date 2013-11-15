6f8102e02b4c884a6a957cef40df473d826e78dd
idueppe <ingo.dueppe@dueppe.com>
2013-11-15 13:03:49 +0100

    Added first implementation of an auctions page.

---
+++ speedauction-jsf/src/main/java/de/swm/speedauction/ui/AuctionController.java
@@ -0,0 +1,30 @@
+package de.swm.speedauction.ui;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import de.swm.auction.dtos.AuctionDTO;
+import de.swm.auction.exceptions.AuctionNotFoundException;
+import de.swm.auction.services.AuctionService;
+
+
+@Controller("auctionController")
+@Scope("session")
+public class AuctionController implements Serializable
+{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private AuctionService auctionService;
+	
+	public List<AuctionDTO> getAuctions() throws AuctionNotFoundException
+	{
+		return auctionService.allAuctions();
+	}
+
+}
